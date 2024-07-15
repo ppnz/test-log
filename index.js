fdd9be29e8f0fc943251bbbadb7f0f8e543ba0a8
Pichavarit Panyokaew <pichavarit_p@tripetch-it.co.th>
2024-07-15 13:40:35 +0700

    fix: error

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
+// index.js
+
+/**
+ * Example function that throws an error.
+ * @throws {Error} Always throws an error with a message.
+ */
 const test = () => {
-    throw ('this error from lib')
-    console.log('test')
+    throw ('This error is from your library');
+    console.log('This line will not be reached');
 }
 
-module.export = {
-test
-}
+// Export the function for use in other modules
+module.exports = {
+    test
+};
